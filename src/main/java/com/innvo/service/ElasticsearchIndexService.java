7219eafc65591f09a747ddb821fe733dd57351bb
ali <ali@ali>
2017-04-20 11:52:10 +0200

    elasticsearch index

---
+++ src/main/java/com/innvo/service/ElasticsearchIndexService.java
@@ -0,0 +1,72 @@
+package com.innvo.service;
+
+import com.codahale.metrics.annotation.Timed;
+import com.innvo.domain.*;
+import com.innvo.repository.*;
+import com.innvo.repository.search.*;
+import org.elasticsearch.indices.IndexAlreadyExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.util.List;
+
+@Service
+public class ElasticsearchIndexService {
+
+    private final Logger log = LoggerFactory.getLogger(ElasticsearchIndexService.class);
+
+    @Inject
+    private ReportRepository reportRepository;
+
+    @Inject
+    private ReportSearchRepository reportSearchRepository;
+
+    @Inject
+    private ReportparameterRepository reportparameterRepository;
+
+    @Inject
+    private ReportparameterSearchRepository reportparameterSearchRepository;
+
+    @Inject
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Async
+    @Timed
+    public void reindexAll() {
+        reindexForClass(Report.class, reportRepository, reportSearchRepository);
+        reindexForClass(Reportparameter.class, reportparameterRepository, reportparameterSearchRepository);
+
+        log.info("Elasticsearch: Successfully performed reindexing");
+    }
+
+    @Transactional(readOnly = true)
+    @SuppressWarnings("unchecked")
+    private <T, ID extends Serializable> void reindexForClass(Class<T> entityClass, JpaRepository<T, ID> jpaRepository,
+                                                              ElasticsearchRepository<T, ID> elasticsearchRepository) {
+        elasticsearchTemplate.deleteIndex(entityClass);
+        try {
+            elasticsearchTemplate.createIndex(entityClass);
+        } catch (IndexAlreadyExistsException e) {
+            // Do nothing. Index was already concurrently recreated by some other service.
+        }
+        elasticsearchTemplate.putMapping(entityClass);
+        if (jpaRepository.count() > 0) {
+            try {
+                Method m = jpaRepository.getClass().getMethod("findAllWithEagerRelationships");
+                elasticsearchRepository.save((List<T>) m.invoke(jpaRepository));
+            } catch (Exception e) {
+                elasticsearchRepository.save(jpaRepository.findAll());
+            }
+        }
+        log.info("Elasticsearch: Indexed all rows for " + entityClass.getSimpleName());
+    }
+}
---
+++ src/main/webapp/app/admin/elasticsearch-reindex/elasticsearch-reindex.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('adapReportApp')
+        .controller('ElasticsearchReindexController', ElasticsearchReindexController);
+
+    ElasticsearchReindexController.$inject = [];
+
+    function ElasticsearchReindexController() {
+    }
+})();
